424b10326305f7ac4b5b6d7bfa4dab418ba51843
doomout <winquake3@hanmail.net>
2022-12-19 15:26:55 +0900

    타이머 구현

--- DatePicker/ViewController.swift
+++ DatePicker/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let timeSelector: Selector = #selector(ViewController.updateTime)
+    let interval = 1.0
+    var count = 0
+    
+    @IBOutlet var lblCurrentTime: UILabel!
+    @IBOutlet var lblPickerTime: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //타이머 설정하기(타이머 간격 | 동작될 뷰  | 타이머 구동될 때 실행할 함수 | 사용자 정보 | 반복 여부)
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
     }
 
-
+    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+        let datePickerView = sender
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
+        lblPickerTime.text = "선택시간: " + formatter.string(from : datePickerView.date)
+    }
+    @objc func updateTime() {
+        lblCurrentTime.text = String(count)
+        count = count + 1
+    }
 }
 
